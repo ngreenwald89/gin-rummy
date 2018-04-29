dd4c8a6774bd83c4c92485cdc22dc5b3139a6d8d
Greenwald <ngreenwald@mitre.org>
2018-04-29 19:16:27 -0400

    add pylint-django to requirements, and use to clean up login_app/

--- login_app/urls.py
+++ login_app/urls.py
@@ -6,7 +6,7 @@ from login_app import views
 app_name = 'login_app'
 
 # Be careful setting the name to just /login use userlogin instead!
-urlpatterns=[
-    url('^register/$',views.register,name='register'),
-    url('^user_login/$',views.user_login,name='user_login'),
+urlpatterns = [
+    url('^register/$', views.register, name='register'),
+    url('^user_login/$', views.user_login, name='user_login'),
 ]
